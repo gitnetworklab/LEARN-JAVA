a2fc5af762ea1bb7efde5a87089bf6ea7b48ae4c
adm-lab-sbodhgir <adm-lab-sbodhgir@email.com>
2024-06-15 12:27:51 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+{
+    "guac": {
+      "type": "rdp",
+      "settings": {
+        "security": "any",
+        "ignore-cert": true,
+        "enable-font-smoothing": true,
+        "enable-wallpaper": true,
+        "enable-theming": true,
+        "enable-full-window-drag": false,
+        "enable-menu-animations": false,
+        "resize-method": "display-update",
+        "server-layout": "en-us-qwerty"
+      }
+    },
+    "kasm_svc": {
+      "port": 4902
+    }
+  }
